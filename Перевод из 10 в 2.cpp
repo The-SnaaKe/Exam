53810ea5868c02d72d57aff4d28693ba4ea8fc7c
Semyon Petrov <93989640+The-SnaaKe@users.noreply.github.com>
2022-06-21 19:53:19 +0500

    Update Перевод из 10 в 2.cpp

--- Перевод из 10 в 2.cpp
+++ Перевод из 10 в 2.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
-int main(){
-    int tenNumber = 0;
-    setlocale(LC_ALL, "RUS");
-    int n;
+int main() {
+	int binaryNumber = 0;
+	int ost, i = 1, step = 1;
+	setlocale(LC_ALL, "RUS");
+	int n;
 
-    cout << "Введите 2чное число: ";
-    cin >> n;
-    int twoNumber = n;
-    int decimalNumber = 0, i = 0, ost;
-    while (n != 0)
-    {
-        ost = n % 10;
-        n /= 10;
-        tenNumber += ost * pow(2, i);
-        ++i;
-    }
-    cout << twoNumber << " в десятиной = " << tenNumber << " в двоичной\n";
+	cout << "Введите 10чное число: ";
+	cin >> n;
+	int tenNumber = n;
+	while (n != 0)
+	{
+		ost = n % 2;
+		cout << "Шаг " << step++ << ": " << n << "/2, Остаток = " << ost << ", Коэффициент = " << n / 2 << endl;
+		n /= 2;
+		binaryNumber += ost * i;
+		i *= 10;
+	}
+	cout << tenNumber << " в десятиной = " << binaryNumber << " в двоичной\n";
 }
