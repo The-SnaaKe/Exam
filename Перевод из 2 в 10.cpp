aa2a0168eeefafcc8a716dd404f3564d8feeb160
Semyon Petrov <93989640+The-SnaaKe@users.noreply.github.com>
2022-06-21 19:51:11 +0500

    Update Перевод из 2 в 10.cpp

--- Перевод из 2 в 10.cpp
+++ Перевод из 2 в 10.cpp
@@ -2,22 +2,21 @@
 #include <cmath>
 using namespace std;
 
-int main()
-{
-    int tenNumber = 0;
-    setlocale(LC_ALL, "RUS");
-    int n;
+int main() {
+	int tenNumber = 0;
+	setlocale(LC_ALL, "RUS");
+	int n;
 
-    cout << "Введите 2чное число: ";
-    cin >> n;
-    int twoNumber = n;
-    int decimalNumber = 0, i = 0, ost;
-    while (n != 0)
-    {
-        ost = n % 10;
-        n /= 10;
-        tenNumber += ost * pow(2, i);
-        ++i;
-    }
-    cout << twoNumber << " в десятиной = " << tenNumber << " в двоичной\n";
+	cout << "Введите 2чное число: ";
+	cin >> n;
+	int twoNumber = n;
+	int ost, i = 0;
+	while (n != 0)
+	{
+		ost = n % 10;
+		n /= 10;
+		tenNumber += ost * pow(2, i);
+		++i;
+	}
+	cout << twoNumber << " в десятиной = " << tenNumber << " в двоичной\n";
 }
